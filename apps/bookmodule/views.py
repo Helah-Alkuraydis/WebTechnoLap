734c6ef34d62c9fd93f8c7ff69f12fcbe041ff94
Helah-Alkuraydis <431201343@qu.edu.sa>
2026-02-23 11:18:22 +0300

    Completed Lab 4 - HTML, CSS, and Django Templates

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,7 +1,7 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse # type: ignore
 
-from django.shortcuts import render 
+from django.shortcuts import render  # type: ignore
 
 # def index(request):
 #     name = request.GET.get("name") or "world!"
@@ -28,3 +28,15 @@ def viewbook(request, bookId):
     if book2['id'] == bookId: targetBook = book2
     context = {'book':targetBook} # book is the variable name accessible by the template
     return render(request, 'bookmodule/show.html', context)
+
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
