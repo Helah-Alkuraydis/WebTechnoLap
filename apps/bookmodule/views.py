a33762664e18d18e788349648de91992768fb6fc
Helah-Alkuraydis <431201343@qu.edu.sa>
2026-04-16 11:16:42 +0300

    Lap 8

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse # type: ignore
 
 from django.shortcuts import render  # type: ignore
 from django.http import HttpResponse
-from .models import Book
+from .models import Book, Address, Student
+from django.db.models import Q
+from django.db.models import Count, Sum, Avg, Max, Min
+
 
 # def index(request):
 #     name = request.GET.get("name") or "world!"
@@ -96,16 +99,47 @@ def insert_book(request):
 
 
 def simple_query(request):
-    mybooks = Book.objects.filter(title__icontains='a') 
+    mybooks = Book.objects.filter(title__icontains='and') 
     return render(request, 'bookmodule/bookList.html', {'books': mybooks})
 
 def complex_query(request):
-    # شوفي كيف عدلنا الشرطات السفلية صارت ثنتين __ عشان جانجو يفهمها صح
     mybooks = Book.objects.filter(author__isnull=False).filter(title__icontains='and').filter(edition__gte=2).exclude(price__lte=100)[:10]
     
-    # هنا نقول: إذا لقيت كتب تطابق الشروط، اعرضها في صفحة bookList
     if len(mybooks) >= 1:
         return render(request, 'bookmodule/bookList.html', {'books': mybooks})
-    # وإذا ما لقيت، ودنا لصفحة index الأساسية
     else:
         return render(request, 'bookmodule/index.html')
+    
+def task1(request):
+    task1_books = Book.objects.filter(Q(price__lte=80.0))
+    return render(request, 'bookmodule/task1.html', {'books': task1_books})
+
+def task2(request):
+    task2_books = Book.objects.filter(
+        Q(edition__gt=3) & (Q(title__icontains='qu') | Q(author__icontains='qu'))
+    )
+    return render(request, 'bookmodule/task2.html', {'books': task2_books})
+
+def task3(request):
+    task3_books = Book.objects.filter(
+        ~Q(edition__gt=3) & ~(Q(title__icontains='qu') | Q(author__icontains='qu'))
+    )
+    return render(request, 'bookmodule/task3.html', {'books': task3_books})
+
+def task4(request):
+    task4_books = Book.objects.order_by('title')
+    return render(request, 'bookmodule/task4.html', {'books': task4_books})
+
+def task5(request):
+    stats = Book.objects.aggregate(
+        my_count=Count('id'),
+        my_sum=Sum('price'),
+        my_avg=Avg('price'),
+        my_max=Max('price'),
+        my_min=Min('price')
+    )
+    return render(request, 'bookmodule/task5.html', {'stats': stats})
+
+def task7(request):
+    cities = Address.objects.annotate(student_count=Count('student'))
+    return render(request, 'bookmodule/task7.html', {'cities': cities})
